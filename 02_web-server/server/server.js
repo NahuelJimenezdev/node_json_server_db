9d376d2aaed407670702a68251ab7280f95dd431
NahuelJimenezDev <102840530+NahuelJimenezdev@users.noreply.github.com>
2024-12-04 14:09:50 -0300

    se modifica el destino de la carpeta public, sacandola de la carpeta web server a la raiz del proyecoy y realizo un servidor web donde servimos una aplicacion tipo res social del pajarito

--- 02_web-server/server/server.js
+++ 02_web-server/server/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
+const { listeners } = require('process')
 
 const startServer = (options) => {
   const { port, public_path = 'public'} = options
-  console.log('port:', port)
-  console.log('public_path', public_path)
+  // console.log('port:', port)
+  // console.log('public_path', public_path)
+
+  const app = express()
+  // Para poder usar middleware se usa la palabra use (express)
+  // los middleware configuran nuestra applicación
+  app.use(express.static(public_path)) // contenido estático que ponemos disponible
+
+  app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname + `../../${public_path}/index.html`)
+    res.sendFile(indexPath)
+  })
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+  })
 }
 
 module.exports = {
